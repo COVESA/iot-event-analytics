60d4aec2e9b43c5d8adc4e3d032a04f0959d0146
Kiefer Lars-Erich <Lars-Erich.Kiefer@de.bosch.com>
2021-03-18 13:49:23 +0100

    Rename protocolAdapter param to protocolGatewayConfig
    Signed-off-by: Kiefer Lars-Erich <Lars-Erich.Kiefer@de.bosch.com>

--- src/core/talent.mr.js
+++ src/core/talent.mr.js
@@ -32,8 +32,8 @@ const {
 } = require('./util/talentIO');
 
 class Mapper extends Talent {
-    constructor(id, reducerId, protocolAdapter) {
-        super(id, protocolAdapter);
+    constructor(id, reducerId, protocolGatewayConfig) {
+        super(id, protocolGatewayConfig);
 
         this.reducerId = reducerId;
 
@@ -169,8 +169,8 @@ Mapper.FEATURE_MAP_ASSIGN = 'map_assign';
 Mapper.FEATURE_MAP_PARTIAL = 'map_partial';
 
 class Worker extends Talent {
-    constructor(id, mapperId, protocolAdapter) {
-        super(id, protocolAdapter);
+    constructor(id, mapperId, protocolGatewayConfig) {
+        super(id, protocolGatewayConfig);
         this.mapperId = mapperId;
     }
 
@@ -226,8 +226,8 @@ class Worker extends Talent {
 Worker.ERROR = 'ERROR';
 
 class Reducer extends Talent {
-    constructor(id, mapperId, protocolAdapter) {
-        super(id, protocolAdapter);
+    constructor(id, mapperId, protocolGatewayConfig) {
+        super(id, protocolGatewayConfig);
 
         this.mapperId = mapperId;
 
