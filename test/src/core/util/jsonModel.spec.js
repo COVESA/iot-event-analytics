076a440f847c5ca9844103aec15cc7e279a27cc4
Kiefer Lars-Erich <Lars-Erich.Kiefer@de.bosch.com>
2021-04-06 22:16:50 +0200

    Add unit tests for JsonModel
    Signed-off-by: Kiefer Lars-Erich <Lars-Erich.Kiefer@de.bosch.com>

--- test/src/core/util/jsonModel.spec.js
+++ test/src/core/util/jsonModel.spec.js
@@ -8,46 +8,41 @@
  * SPDX-License-Identifier: MPL-2.0
  ****************************************************************************/
 
- const JsonModel = require('../../../../src/core/util/jsonModel');
+const JsonModel = require('../../../../src/core/util/jsonModel');
 
- describe('core.util.jsonModel', () => {
+describe('core.util.jsonModel', () => {
     let json = null;
     let model = null;
 
-     beforeEach(() => {
-        json = {
-            bar: {
-                baz: 'Hello World',
-                'bla.blubb': 555
-            }
-        }
+    beforeEach(() => {
+        json = require('../../../resources/jsonModel.input.json');
         model = new JsonModel(json);
-     });
+    });
 
-     it('shold return a given value', () => {
+    it('shold return a given value', () => {
         expect(model.get('bar.baz')).toBe('Hello World');
-     });
+    });
 
-     it('shold return the default value in case the path does not exist', () => {
+    it('shold return the default value in case the path does not exist', () => {
         expect(model.get('bar.bar', 'Hello')).toBe('Hello');
-     });
+    });
 
-     it('should throw an error on a given false path without a default value', () => {
+    it('should throw an error on a given false path without a default value', () => {
         expect(() => model.get('bar.bar')).toThrow();
-     });
+    });
 
-     it('it should validate a model, with a given validator function', () => {
+    it('it should validate a model, with a given validator function', () => {
         expect(() => {
             new JsonModel(json, () => {
                 return false;
             });
         }).toThrow();
-     });
+    });
 
-     it('shold return a submodel at a given path', () => {
+    it('shold return a submodel at a given path', () => {
         const submodel = model.getSubmodel('bar');
 
         expect(submodel instanceof JsonModel).toBeTruthy();
         expect(submodel.get("'bla.blubb'")).toBe(555);
-     });
- });
+    });
+});
