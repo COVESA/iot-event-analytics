7c5d95b763f3fbbf43209f7cfc93aeea80cb5940
Maria Ivanova <maria.ivanova@bosch.io>
2021-04-14 11:57:33 +0300

    Add jsdoc for core/util/clone module.
    Signed-off-by: Maria Ivanova <maria.ivanova@bosch.io>

--- src/core/util/clone.js
+++ src/core/util/clone.js
@@ -8,6 +8,24 @@
  * SPDX-License-Identifier: MPL-2.0
  ****************************************************************************/
 
+/**
+ * Clone Module.
+ * 
+ * @module clone 
+ */
+
+
+/**
+ * Performs a deep-clone of the passed object. Clones also the property values of the object including the inherited
+ * ones.
+ *
+ * @param {object} value - Object to be cloned.
+ * @param {*} [createKey = (key => key)] - A function defining how to treat the object keys (properties) during the
+ * clone.
+ * @param {*} [exclude = (key => false)] - A function defining if an object key (property) should be excluded from the
+ * cloning.
+ * @returns a clone of the _value_ object.
+ */
 module.exports = function clone(value, createKey = (key => key), exclude = (key => false)) {
     if (Array.isArray(value)) {
         const arr = [];
