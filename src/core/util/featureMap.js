f4596403cb6c52c733fa3f26c7a8b2edd9755879
Maria Ivanova <maria.ivanova@bosch.io>
2021-04-14 12:00:25 +0300

    Add jsdoc for core/util/featureMap module.
    Signed-off-by: Maria Ivanova <maria.ivanova@bosch.io>

--- src/core/util/featureMap.js
+++ src/core/util/featureMap.js
@@ -8,20 +8,39 @@
  * SPDX-License-Identifier: MPL-2.0
  ****************************************************************************/
 
-// Stores entries in following hierarchical order
-// <type>.<feature>.<instanceId>.{ $metadata, }
 
-module.exports = class FeatureMap {
+/**
+ * Module Feature Map.
+ * 
+ * @module featureMap
+ */
+
+/**
+ * This is a utility class for keeping features organized. Stores entries in the following hierarchical order
+ * \<type\>.\<feature\>.\<instanceId\>.{ $metadata, }.
+ */
+ class FeatureMap {
+    /**
+     * Constructs a FeatureMap instance with no entries.
+     */
     constructor() {
         this.clear();
     }
 
+    /**
+     * Checks if the FeatureMap contains an entry with the specified _type_, _feature_ and _instanceId_ properties.
+     *
+     * @param {*} type - Type that the feature belongs to.
+     * @param {*} feature - Feature name.
+     * @param {*} instanceId - Instance Id.
+     * @returns true if the map contains the specified feature.
+     */
     contains(type, feature, instanceId) {
         try {
             this.entries[type][feature][instanceId].$feature;
             return true;
         }
-        catch(err) {
+        catch (err) {
             return false;
         }
     }
@@ -30,6 +49,16 @@ module.exports = class FeatureMap {
         this.entries = {};
     }
 
+    /**
+     * Sets a value of an entry in the map. The entry is identified by _type_, _feature_ and _instanceId_ parameters.
+     * If this entry is already in the map, it will be overwritten.
+     * 
+     * @param {*} type - Type that the feature belongs to.
+     * @param {*} feature - Feature name.
+     * @param {*} instanceId - Instance Id.
+     * @param {*} $feature - Value of the feature.
+     * @param {*} $metaFeature - Metadata for the feature.
+     */
     set(type, feature, instanceId, $feature, $metaFeature) {
         const featureEntry = this.__createFeatureEntryIfNeeded(type, feature);
 
@@ -44,6 +73,15 @@ module.exports = class FeatureMap {
         featureEntry[instanceId] = { $feature, matches };
     }
 
+    /**
+     * Gets the entry in the map that can be identified by _type_, _feature_ and _instanceId_ parameters.
+     * 
+     * @param {*} type - Type that the feature belongs to.
+     * @param {*} feature - Feature name.
+     * @param {*} instanceId - Instance Id.
+     * @returns the matching entry value, previously set by {@link module:featureMap~FeatureMap#set}.
+     * @throws an Error in case the entry can not be found.
+     */
     get(type, feature, instanceId) {
         if (this.contains(type, feature, instanceId)) {
             return this.entries[type][feature][instanceId];
@@ -52,16 +90,28 @@ module.exports = class FeatureMap {
         throw new Error(`Could not find FeatureMap entry with type=${type}, feature=${feature}, instanceId=${instanceId}`);
     }
 
+    /**
+     * When a rule matches against a feature it can record the match using this method.
+     * 
+     * @param {*} type - Type that the feature belongs to.
+     * @param {*} feature - Feature name.
+     * @param {*} instanceId - Instance Id.
+     */
     recordRuleMatchFor(type, feature, instanceId) {
         try {
             this.get(type, feature, instanceId).matches++;
         }
-        catch(err) {
+        catch (err) {
             // Match cannot be recorded
             // Omit this
         }
     }
 
+    /**
+     * Gets a reference to the entries of the feature map.
+     * 
+     * @returns object with map entries.
+     */
     dump() {
         return this.entries;
     }
@@ -83,4 +133,6 @@ module.exports = class FeatureMap {
 
         return $feature;
     }
-};
+}
+
+module.exports = FeatureMap;
