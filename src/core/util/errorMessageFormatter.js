912ea099a785fb757a41c450e020e9f0dcca8cb9
Maria Ivanova <maria.ivanova@bosch.io>
2021-04-14 11:58:49 +0300

    Add jsdoc for core/util/errorMessageFormatter module.
    Signed-off-by: Maria Ivanova <maria.ivanova@bosch.io>

--- src/core/util/errorMessageFormatter.js
+++ src/core/util/errorMessageFormatter.js
@@ -1,7 +1,21 @@
 const { ValidationError } = require('ajv')
+/**
+ * Module Error Message Formatter.
+ * 
+ * @module errorMessageFormatter
+ */
 
+/**
+ * ErrorMessageFormatter class.
+ */
 class ErrorMessageFormatter {}
 
+/**
+ * Formats an AJV ValidationError object into a string ready to be put in a log message.
+ *
+ * @param {object} validationError AJV ValidationError object.
+ * @returns a formatted string message if the passed argument is an AJV ValidationError object, validationError.message otherwise.
+ */
 ErrorMessageFormatter.formatAjvValidationError = function formatAjvValidationError(validationError) {
     if (validationError instanceof ValidationError) {
         return validationError.errors.map(err => `${err.dataPath} ${err.message}`).join(', ')
