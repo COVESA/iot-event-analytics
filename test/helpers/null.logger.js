2304e5c11926b4bff9a8286b32aa0b20f2d3d4b3
Kiefer Lars-Erich <Lars-Erich.Kiefer@de.bosch.com>
2021-04-15 11:32:14 +0200

    Prevent logging output during unit-tests
    Signed-off-by: Kiefer Lars-Erich <Lars-Erich.Kiefer@de.bosch.com>

--- test/helpers/null.logger.js
+++ test/helpers/null.logger.js
@@ -1,8 +1,8 @@
 const Logger = require('../../src/core/util/logger');
-
+Logger.__LOG_LEVEL.ALWAYS = 0;
 module.exports = class NullLogger extends Logger {
     constructor(name) {
-        super(name)
+        super(name);
     }
 
     __log() {}
