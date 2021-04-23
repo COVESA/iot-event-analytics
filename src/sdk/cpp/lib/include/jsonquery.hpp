4dd98507590e4a732d9986fb4695269dea980ab0
Alexander Perlman <fixed-term.Alexander.Perlman@se.bosch.com>
2021-04-23 06:24:39 +0200

    Add JSON query for the C++ SDK
    Signed-off-by: Alexander Perlman <fixed-term.Alexander.Perlman@se.bosch.com>

---
+++ src/sdk/cpp/lib/include/jsonquery.hpp
@@ -0,0 +1,260 @@
+/*****************************************************************************
+ * Copyright (c) 2021 Bosch.IO GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ ****************************************************************************/
+
+#ifndef IOTEA_JSONQUERY_HPP
+#define IOTEA_JSONQUERY_HPP
+
+#include <ostream>
+#include <regex>
+#include <string>
+#include <vector>
+
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+namespace iotea {
+namespace core {
+
+/**
+ * @brief A very simple tokenizer used for parsing JSON queries
+ */
+class Tokenizer {
+   public:
+    /**
+     * @brief Construct a new Tokenizer object.
+     *
+     * @param str A string to tokenizer
+     * @param delimiters A string containing token delimiters
+     */
+    Tokenizer(const std::string& str, const std::string& delimiters);
+
+    /**
+     * @brief Get the next token.
+     *
+     * @return std::string
+     */
+    std::string Next();
+
+    /**
+     * @brief Push back token last returned by Next(). At least on call to
+     * Next() must preceed one call to PushBack().
+     */
+    void PushBack();
+
+    /**
+     * @brief Tell whether this tokenizer has more more tokens.
+     *
+     * @return bool
+     */
+    bool HasNext() const;
+
+   private:
+    friend std::ostream& operator<<(std::ostream& os, const Tokenizer& t);
+
+    const std::string str_;
+    const std::string delimiters_;
+    std::string word_;
+    std::string last_;
+    std::size_t idx_ = 0;
+};
+
+/**
+ * @brief QueryResult represents a single JSON query result.
+ */
+class QueryResult {
+   public:
+    /**
+     * @brief Construct a new QueryResult object.
+     *
+     * @param query The resolved JSON query
+     * @param label The label associated with the query
+     * @param value The query result value
+     */
+    QueryResult(const std::string& query, const std::string& label, const json& value);
+
+    /**
+     * @brief Compare two QueryResults for equality.
+     *
+     * @return bool
+     */
+    bool operator==(const QueryResult& other) const;
+
+
+    /**
+     * @brief Get the resolved JSON query.
+     *
+     * @return std::string
+     */
+    std::string GetQuery() const;
+
+    /**
+     * @brief Get the label associated with the query.
+     *
+     * @return std:.string
+     */
+    std::string GetLabel() const;
+
+    /**
+     * @brief Get the resulting query value
+     *
+     * @return json
+     */
+    json GetValue() const;
+
+   private:
+    friend std::ostream& operator<<(std::ostream& os, const QueryResult& r);
+
+    std::string query_;
+    std::string label_;
+    json value_;
+};
+
+class JsonQueryException : public std::exception {
+   public:
+       enum class Code {
+           KEY_NOT_FOUND,
+           INVALID_RANGE_PARAMETER,
+           INVALID_RANGE,
+           OUT_OF_BOUNDS,
+           UNTERMINATED_RANGE,
+           INVALID_QUERY,
+       };
+
+       explicit JsonQueryException(const std::string& msg, const Code& code);
+
+       virtual ~JsonQueryException() = default;
+
+       virtual const char* what() const noexcept;
+
+       virtual Code GetCode() const noexcept;
+
+   private:
+    std::string msg_;
+    Code code_;
+};
+
+/**
+ * @brief JsonQuery extracts values from a JSON object using a specialized query syntax.
+ *
+ * A query consists of one or more period delimited positional attributes and
+ * ranges followed by a single label.
+ *
+ * Attribute:
+ *  - A literal JSON object key
+ *  - A single quoted JSON object key, required if the key contains any of the characters .[]:
+ *    Attributes containing single quotes are not supported.
+ *  - A '*' wildcard meaning any key at the given position should be included in the result.
+ *
+ * Range:
+ *  - [:]   extract all elements in an array
+ *  - [N]   extract the N:th element in an array
+ *  - [-N]  extract the N:th element counting from the end of the array, -1
+ *          denotes the last element of the array, -2 the second to last element and so on.
+ *  - [M:N] extract elements M through N (not inclusive). If M=N an empty result is returned.
+ *          If M > N an exception is thrown
+ *
+ * Label:
+ *  - :<label> where <label> is a string string of non zero length not
+ *             containing any of the characters .[]:'
+ *
+ * Example 1:
+ * Given the object
+ *
+ * @code
+    {
+        "foo": { "bar": "baz" } 
+    }
+ * @endcode
+ *
+ * The query "foo.bar:my_label" would return
+ *
+ * @code
+    std::vector<QueryResult>{
+        QueryResult{"foo.bar", "my_label", json("baz")}
+    }
+ * @endcode
+ *
+ * Example 2:
+ * Given the object
+ *
+ * @code
+    {
+        "foo": {
+            "bar": {
+                "alpha": [
+                    { "foo.bar": 1, "bar.foo": 2 },
+                    { "foo.bar": 3, "bar.foo": 4 },
+                    { "foo.bar": 5, "bar.foo": 6 },
+                    { "foo.bar": 7, "bar.foo": 8 }
+                ],
+                "beta": [
+                    { "foo.bar": 9, "bar.foo": 10 },
+                    { "foo.bar": 11, "bar.foo": 12 },
+                    { "foo.bar": 13, "bar.foo": 14 },
+                    { "foo.bar": 15, "bar.foo": 16 }
+                ]
+            }
+        }
+    }
+ * @endcode
+ *
+ * The query "foo.bar.*[1:3].'foo.bar':my_label" would return
+ *
+ * @code
+    std::vector<QueryResult>{
+        QueryResult{"foo.bar.alpha[1].'foo.bar'", "my_label", json(3)},
+        QueryResult{"foo.bar.alpha[2].'foo.bar'", "my_label", json(5)},
+        QueryResult{"foo.bar.beta[1].'foo.bar'", "my_label", json(11)},
+        QueryResult{"foo.bar.beta[2].'foo.bar'", "my_label", json(13)}
+    }
+ * @endcode
+ */
+class JsonQuery {
+   public:
+    /**
+     * @brief Construct a new QueryParser object.
+     * 
+     * @param query The JSON query
+     */
+    explicit JsonQuery(const std::string& query);
+
+    /**
+     * @brief Apply query to a JSON object.
+     *
+     * @param value The JSON object to query
+     * @return std::vector<QueryResult>
+     */
+    std::vector<QueryResult> Query(const json& value);
+
+   private:
+    enum class State {
+        PARSE_INIT,
+        PARSE_ATTR,
+        PARSE_SEPARATOR,
+        PARSE_QUOTE,
+        PARSE_RANGE_OPEN,
+        PARSE_RANGE,
+        PARSE_RANGE_CLOSE,
+        PARSE_LABEL,
+    };
+
+    std::string StateToString(const State& state) const;
+    bool StringToInt(const std::string& str, int& val);
+    std::vector<QueryResult> ParseInternal(State state, Tokenizer tok, std::string path, const json& value);
+
+    const std::string query_;
+
+};
+
+}  // namespace core
+}  // namespace iotea
+
+#endif // IOTEA_JSONQUERY_HPP
---
+++ src/sdk/cpp/lib/src/jsonquery.cpp
@@ -0,0 +1,362 @@
+/*****************************************************************************
+ * Copyright (c) 2021 Bosch.IO GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ ****************************************************************************/
+
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "nlohmann/json.hpp"
+
+#include "jsonquery.hpp"
+#include "logging.hpp"
+
+using json = nlohmann::json;
+
+namespace iotea {
+namespace core {
+
+QueryResult::QueryResult(const std::string& query, const std::string& label, const json& value)
+    : query_{query}
+    , label_{label}
+    , value_(value) {
+}
+
+bool QueryResult::operator==(const QueryResult& other) const {
+    return query_ == other.query_ &&
+        label_ == other.label_ &&
+        value_ == other.value_;
+}
+
+std::ostream& operator<<(std::ostream& os, const QueryResult& r) {
+    return os <<
+        "{\"query\": \"" << r.GetQuery() <<
+        "\", \"label\": \"" << r.GetLabel() <<
+        "\", \"value\": " << r.GetValue().dump() <<
+        "}";
+}
+
+std::string QueryResult::GetQuery() const {
+    return query_;
+}
+
+std::string QueryResult::GetLabel() const {
+    return label_;
+}
+
+json QueryResult::GetValue() const {
+    return value_;
+}
+
+JsonQueryException::JsonQueryException(const std::string& msg, const JsonQueryException::Code& code)
+    : std::exception{}
+    , msg_{msg}
+    , code_{code} {}
+
+const char* JsonQueryException::what() const noexcept {
+    return msg_.c_str();
+}
+
+JsonQueryException::Code JsonQueryException::GetCode() const noexcept {
+    return code_;
+}
+
+JsonQuery::JsonQuery(const std::string& query)
+    : query_{query} {}
+
+bool JsonQuery::StringToInt(const std::string& str, int& val) {
+    try {
+        val = std::stoi(str);
+    } catch (const std::invalid_argument& e) {
+        return false;
+    }
+
+    return true;
+}
+
+std::string JsonQuery::StateToString(const State& state) const {
+    switch (state) {
+        case State::PARSE_INIT: return "PARSE_INIT";
+        case State::PARSE_ATTR: return "PARSE_ATTR";
+        case State::PARSE_SEPARATOR: return "PARSE_SEPARATOR";
+        case State::PARSE_QUOTE: return "PARSE_QUOTE";
+        case State::PARSE_RANGE_OPEN: return "PARSE_RANGE_OPEN";
+        case State::PARSE_RANGE: return "PARSE_RANGE";
+        case State::PARSE_RANGE_CLOSE: return "PARSE_RANGE_CLOSE";
+        case State::PARSE_LABEL: return "PARSE_LABEL";
+        default: throw std::logic_error("should not happen");
+    }
+}
+
+std::vector<QueryResult> JsonQuery::ParseInternal(State state, Tokenizer tok, std::string path, const json& value) {
+    std::vector<QueryResult> results;
+    std::string sym;
+    std::string quoted_key;
+    int range_from;
+    int range_to;
+
+    while (tok.HasNext()) {
+        sym = tok.Next();
+
+        switch (state) {
+            case State::PARSE_INIT: {
+                // The first symbol in a query must be one of
+                // - a wildcard "*"
+                // - a single quote "'"
+                // - a key found in value
+
+                if (sym == "*" || sym == "'" || value.contains(sym)) {
+                    tok.PushBack();
+                    state = State::PARSE_ATTR;
+                    break;
+                }
+
+                throw JsonQueryException("invalid query", JsonQueryException::Code::INVALID_QUERY);
+            }
+            case State::PARSE_ATTR: {
+                // A valid attribute is one of
+                // - a wildcard "*"
+                // - a single quote "'"
+                // - a key found in value
+                //
+                // If sym is neither of these it should be treated as a separator
+
+                if (sym == "*") {
+                    for (const auto& v : value.items()) {
+                        auto sub_results = ParseInternal(State::PARSE_SEPARATOR, tok, path + v.key(), v.value());
+                        results.insert(results.end(), sub_results.begin(), sub_results.end());
+                    }
+
+                    return results;
+                }
+
+                if (sym == "'") {
+                    quoted_key.clear();
+                    state = State::PARSE_QUOTE;
+                    break;
+                }
+
+                if (value.contains(sym)) {
+                    auto sub_results = ParseInternal(State::PARSE_SEPARATOR, tok, path + sym, value[sym]);
+                    results.insert(results.end(), sub_results.begin(), sub_results.end());
+
+                    return results;
+                }
+
+                tok.PushBack();
+                state = State::PARSE_SEPARATOR;
+                break;
+            }
+            case State::PARSE_QUOTE: {
+                if (sym != "'") {
+                    quoted_key += sym;
+                    break;
+                }
+
+                auto quoted = "'" + quoted_key + "'";
+                auto sub_results = ParseInternal(State::PARSE_SEPARATOR, tok, path + quoted, value[quoted_key]);
+                results.insert(results.end(), sub_results.begin(), sub_results.end());
+
+                return results;
+            }
+            case State::PARSE_SEPARATOR: {
+                // A speparator must be one of
+                // - a period "." -> expect an attribute to follow
+                // - an opening square bracket "[" -> expect a range to follow
+                // - a colon ":" -> expect a label to follow
+
+                if (sym == ".") {
+                    path += ".";
+                    state = State::PARSE_ATTR;
+                    break;
+                }
+
+                if (sym == "[") {
+                    state = State::PARSE_RANGE_OPEN;
+                    break;
+                }
+
+                if (sym == ":") {
+                    state = State::PARSE_LABEL;
+                    break;
+                }
+
+                throw JsonQueryException{"missing separator", JsonQueryException::Code::INVALID_QUERY};
+            }
+            case State::PARSE_RANGE_OPEN: {
+                // A range must contain either
+                // - a colon ":" -> the entire array, expect a closing square bracket to follow
+                // - an integer -> a specific element or the beginning of a range, continue parsing the range
+                if (sym == ":") {
+                    // Range given as [:
+                    range_from = 0;
+                    range_to = value.size();
+                    state = State::PARSE_RANGE_CLOSE;
+                    break;
+                }
+
+                // Expect range to begin as [M
+                if (!StringToInt(sym, range_from)) {
+                    throw JsonQueryException{"range parameters must be integers", JsonQueryException::Code::INVALID_RANGE_PARAMETER};
+                }
+
+                state = State::PARSE_RANGE;
+                break;
+            }
+            case State::PARSE_RANGE: {
+                // The "insides" of a range must either
+                // - end immediately with a closing square bracket "]", push back and handle the "close" in a separate state
+                // - contain a colon ":" followed by an integer representing the end of the range, expect a "close"
+                auto size = static_cast<int>(value.size());
+
+                // Range given as [M]
+                if (sym == "]") {
+                    tok.PushBack();
+
+                    if (range_from < -size || range_from >= size) {
+                        throw JsonQueryException{"range start is out of bounds", JsonQueryException::Code::OUT_OF_BOUNDS};
+                    }
+
+                    if (range_from < 0) {
+                        range_from = size + range_from;
+                    }
+
+                    range_to = range_from + 1;
+                    state = State::PARSE_RANGE_CLOSE;
+                    break;
+                }
+
+                // Range given as [M:N
+                if (sym == ":") {
+                    // Remain in this state
+                    break;
+                }
+
+                // Expect range to be given as [M:N]
+                if (!StringToInt(sym, range_to)) {
+                    throw JsonQueryException{"range parameters must be integers", JsonQueryException::Code::INVALID_RANGE_PARAMETER};
+                }
+
+                if (range_to < -size || range_to > size) {
+                    throw JsonQueryException{"range end is out of bounds", JsonQueryException::Code::OUT_OF_BOUNDS};
+                }
+
+                if (range_to < 0) {
+                    range_to = size + range_to;
+                }
+
+                if (range_from > range_to) {
+                    throw JsonQueryException{"range start is after range end", JsonQueryException::Code::INVALID_RANGE};
+                }
+
+                state = State::PARSE_RANGE_CLOSE;
+                break;
+            }
+            case State::PARSE_RANGE_CLOSE: {
+                // A range close must begin with a closing square brace "]". Expect an attribute, a range or a label to follow
+                if (sym != "]") {
+                    throw JsonQueryException{"range not terminated", JsonQueryException::Code::UNTERMINATED_RANGE};
+                }
+
+                if (range_from == range_to) {
+                    // An empty range yields an empty result
+                    return {};
+                }
+
+                for (int i = range_from; i < range_to; i++) {
+                    auto range = "[" + std::to_string(i) + "]";
+                    auto sub_result = ParseInternal(State::PARSE_ATTR, tok, path + range, value[i]);
+                    results.insert(results.end(), sub_result.begin(), sub_result.end());
+                }
+
+                return results;
+            }
+            case State::PARSE_LABEL: {
+                // A label is any string i.e. everything that follows the label
+                // colon. This is a terminal state in which the QueryResult is
+                // created.
+                auto label = tok.Next();
+                while (tok.HasNext()) {
+                    label += tok.Next();
+                }
+
+                results.insert(results.end(), QueryResult{path, label, value});
+
+                return results;
+            }
+
+            default:
+                throw std::logic_error("invalid state");
+        }
+    }
+
+    // Any valid query will terminate in the PARSE_LABEL state
+    throw JsonQueryException{"invalid query", JsonQueryException::Code::INVALID_QUERY};
+}
+
+std::vector<QueryResult> JsonQuery::Query(const json& value) {
+    auto tok = Tokenizer{query_, ".:'[]"};
+
+    return ParseInternal(State::PARSE_INIT, tok, "", value);
+}
+
+Tokenizer::Tokenizer(const std::string& str, const std::string& delimiters)
+    : str_{str}
+    , delimiters_{delimiters}
+    , idx_{0} {}
+
+std::string Tokenizer::Next() {
+    while (idx_ < str_.size()) {
+        auto c = str_[idx_++];
+
+        if (delimiters_.find(c) != std::string::npos) {
+            if (word_.empty()) {
+                last_ = std::string{c};
+                return last_;
+            }
+
+            idx_--;
+
+            last_ = word_;
+            word_.clear();
+            return last_;
+        }
+
+        word_.push_back(c);
+    }
+
+    last_ = word_;
+    return word_;
+}
+
+void Tokenizer::PushBack() {
+    // TODO PushBack() is only allowed once between each Next()
+    // Throw exception in case of consecutive PushBack()s
+    if (last_.size() == 0) {
+        throw std::logic_error("nothing to push back");
+    }
+
+    idx_ -= last_.size();
+
+    word_.clear();
+    last_.clear();
+}
+
+bool Tokenizer::HasNext() const {
+    return idx_ < str_.size();
+}
+
+std::ostream& operator<<(std::ostream& os, const Tokenizer& t) {
+    return os << t.str_.substr(t.idx_) << "\n";
+}
+
+}  // namespace core
+}  // namespace iotea
