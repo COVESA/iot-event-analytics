bcc3f9ff412c6d19333531d993c8eddfba8a2876
Kiefer Lars-Erich <Lars-Erich.Kiefer@de.bosch.com>
2021-04-23 16:08:14 +0200

    Add unit tests for talent module
    Signed-off-by: Kiefer Lars-Erich <Lars-Erich.Kiefer@de.bosch.com>

---
+++ src/sdk/python/tests/iotea/core/test_talent.py
@@ -0,0 +1,35 @@
+import os
+import pytest
+from unittest import TestCase
+from src.iotea.core.talent import Talent
+from src.iotea.core.rules import AndRules, Rule, ChangeConstraint, Constraint
+from tests.helpers.json_loader import load_json
+
+@pytest.fixture
+def test_case():
+    return TestCase()
+
+class MyTalent(Talent):
+    def __init__(self):
+        super(MyTalent, self).__init__('test-talent', 'mqtt://localhost:1883')
+
+    def get_rules(self):
+        return AndRules([
+            Rule(ChangeConstraint('temp', 'kuehlschrank', Constraint.VALUE_TYPE['RAW']))
+        ])
+
+    async def on_event(self, ev, evtctx):
+        print(f'Raw value {TalentInput.get_raw_value(ev)}')
+
+@pytest.fixture
+def talent():
+    talent = MyTalent()
+    yield talent
+    # Teardown logic
+
+class TestTalent:
+    def test_create_discovery_response(self, talent, test_case):
+        test_case.assertDictEqual(
+            talent._Talent__create_discovery_response(),
+            load_json(os.path.realpath('tests/resources/talent.discovery-response.json'))
+        )
