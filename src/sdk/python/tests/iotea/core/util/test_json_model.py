673aae23a2697a09fcd5ac68d348c524dfc43b1e
Kiefer Lars-Erich <Lars-Erich.Kiefer@de.bosch.com>
2021-04-23 16:06:45 +0200

    Add unit tests for json_model
    Signed-off-by: Kiefer Lars-Erich <Lars-Erich.Kiefer@de.bosch.com>

---
+++ src/sdk/python/tests/iotea/core/util/test_json_model.py
@@ -0,0 +1,27 @@
+import os
+import pytest
+from unittest import TestCase
+from src.iotea.core.util.json_model import JsonModel
+from tests.helpers.json_loader import load_json
+
+@pytest.fixture
+def test_case():
+    return TestCase()
+
+@pytest.fixture
+def model():
+    json = load_json(os.path.realpath('tests/resources/jsonModel.input.json'))
+    return JsonModel(json)
+
+class TestJsonModel:
+    def test_return_a_given_value(self, model):
+        assert model.get('bar.baz') == 'Hello World'
+
+    def test_return_default_value(self, model):
+        assert model.get('bar.bar', 'Hello') == 'Hello'
+
+    def test_raise_error_on_given_false_path(self, test_case, model):
+        with pytest.raises(Exception) as exc_info:
+            model.get('bar.bar')
+
+        assert 'Path bar does not exist' == str(exc_info.value)
